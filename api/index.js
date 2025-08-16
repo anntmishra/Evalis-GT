82927f1688e8a8a56a96987951909ed3bc631534
Anant Mishra <144559155+anntmishra@users.noreply.github.com>
2025-08-17 04:42:39 +0530

    Add minimal serverless handler for debugging

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 // Vercel serverless function handler
 // This file routes all API requests to our Express serverless handler
 
-const app = require('../server/serverless');
+// Use minimal version for debugging
+const app = require('../server/serverless-minimal');
 
 module.exports = app;
